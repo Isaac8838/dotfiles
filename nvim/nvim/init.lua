76474cf0ab94bd3be60c992c96a21bfeb40fffc8
Isaac8838 <77573391+Isaac8838@users.noreply.github.com>
2024-08-24 17:14:25 +0800

    config nvim

---
+++ nvim/nvim/init.lua
@@ -0,0 +1,23 @@
+-- lazy install
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- vim options
+require("vim-options")
+
+-- lazy
+require("lazy").setup("plugins")
+
+--theme
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
